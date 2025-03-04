fd0c8fde993a23ef1cf547296b2d4b3baf685734
Shawon <mdmoinulhossainshawon@gmail.com>
2025-03-04 10:45:14 +0600

    feat(plugins): Added support for remote URL for custom parsers

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -6,11 +6,33 @@ return {
 	opts = {},
 
 	config = function ()
+		--- Gets the path(or URL) for the parser.
+		---@param path string
+		---@param url string
+		---@return string
+		local function get_path(path, url)
+			---@type table Path stat.
+			local stat = vim.uv.fs_stat(vim.fn.stdpath("config") .. path);
+
+			if stat and stat.type == "directory" then
+				return vim.fn.stdpath("config") .. path;
+			else
+				return url;
+			end
+		end
+
 		local parser_configs = require("nvim-treesitter.parsers").get_parser_configs();
 
 		parser_configs.lua_patterns = {
 			install_info = {
-				url = vim.fn.stdpath("config") .. "/parsers/tree-sitter-lua_patterns",
+				url = get_path("/parsers/tree-sitter-lua_patterns", "https://github.com/OXY2DEV/tree-sitter-lua_patterns.git"),
+				files = { "src/parser.c" }
+			}
+		};
+
+		parser_configs.vhs = {
+			install_info = {
+				url = get_path("/parsers/tree-sitter-vhs", "https://github.com/OXY2DEV/tree-sitter-vhs.git"),
 				files = { "src/parser.c" }
 			}
 		};
@@ -23,12 +45,13 @@ return {
 				"regex",
 				"markdown",
 				"markdown_inline",
-				"typst",
-				"latex",
 				"yaml",
 
 				"vimdoc",
-				"query"
+				"query",
+
+				"lua_patterns",
+				"vhs"
 			},
 			highlight = {
 				enable = true,

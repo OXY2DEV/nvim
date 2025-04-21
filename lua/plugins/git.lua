5d36d713dddbe71a8ee054e5f2f3092819f5477b
Shawon <mdmoinulhossainshawon@gmail.com>
2025-04-22 00:53:40 +0600

    chore: Updated config

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -22,7 +22,7 @@ return {
 			current_line_blame_opts = {
 				virt_text_pos = "right_align",
 			},
-			current_line_blame_formatter = " <author>, 󰔚 <author_time:%R> |  <summary>",
+			current_line_blame_formatter = " <author>, 󰔚 <author_time:%c>",
 			update_debounce = 200,
 		}
 	}

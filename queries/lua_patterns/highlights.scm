a49782f98d2b8cb56fa57cafb3a7bbd7e010ecf2
Shawon <mdmoinulhossainshawon@gmail.com>
2025-04-28 22:33:07 +0600

    fix(injections): Fixed lua_pattern injections for lua
    fix(highlights): Fixed highlights for lua_patterns

--- queries/lua_patterns/highlights.scm
+++ queries/lua_patterns/highlights.scm
@@ -1,3 +1,29 @@
+(escape_sequence) @character.special
+
+(literal_character) @character
+
+(character_class) @variable.builtin
+
+(escaped_character) @string.escape
+
+(any_character) @variable.member
+
+(character_reference) @constant.builtin
+
+[
+ (start_assertion)
+ (end_assertion)
+] @keyword
+
+
+[
+ (zero_or_more)
+ (one_or_more)
+ (optional)
+
+ (lazy)
+] @keyword.operator
+
 [
   "("
   ")"
@@ -5,29 +31,10 @@
   "]"
 ] @punctuation.bracket
 
-[
-  (escaped_character)
-  (start_assertion)
-  (end_assertion)
-  (escape_sequence)
-] @string.escape
-
-[
-  (zero_or_more)
-  (one_or_more)
-  (lazy)
-  (optional)
-] @operator
-
 (character_set
   [
     "^" @operator
     (character_range
       "-" @operator)
   ])
-
-(literal_character) @string
-
-(character_set_content
-  (literal_character) @constant.character)
 

054c729587175833e41bc580f1115d3db4a822ce
Shawon <mdmoinulhossainshawon@gmail.com>
2025-05-08 08:02:58 +0600

    feat(blink): Markdown preview for the documentation window in blink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -126,59 +126,79 @@ return {
 					}
 				},
 				documentation = {
-					window = { border = 'single' },
-					-- draw = function (data)
-					-- 	---|fS
-					--
-					-- 	if not data.item.documentation then
-					-- 		-- Documentation not available.
-					-- 		return;
-					-- 	elseif data.item.documentation.kind ~= "markdown" then
-					-- 		-- Documentation isn't in markdown.
-					-- 		data.default_implementation();
-					-- 		return;
-					-- 	elseif package.loaded["markview"] == nil then
-					-- 		-- markview.nvim not available.
-					-- 		data.default_implementation();
-					-- 		return;
-					-- 	end
-					--
-					-- 	---@type integer
-					-- 	local buf = data.window.buf;
-					-- 	---@type string[]
-					-- 	local lines = vim.split(data.item.documentation.value or "", "\n", { trimempty = true });
-					-- 	---@type string[]
-					-- 	local details = vim.split(data.item.detail or "", "\n", { trimempty = false });
-					-- 	table.insert(details, "");
-					--
-					-- 	vim.print(data)
-					-- 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.list_extend(details, lines));
-					--
-					-- 	if vim.g.__reg_doc ~= true then
-					-- 		vim.treesitter.language.register("markdown", "blink-cmp-documentation");
-					-- 		vim.g.__reg_doc = true;
-					-- 	end
-					--
-					-- 	if package.loaded["markview"] then
-					-- 		local utils = package.loaded["markview.utils"];
-					-- 		vim.defer_fn(function ()
-					-- 			local window = utils.buf_getwin(buf);
-					-- 			vim.wo[window].signcolumn = "no";
-					-- 			vim.api.nvim_win_set_config(window, {
-					-- 				border = "rounded",
-					-- 			});
-					--
-					-- 			require("markview").strict_render:clear(buf);
-					-- 			require("markview").strict_render:render(buf, 999);
-					-- 		end, 25)
-					-- 	end
-					--
-					-- 	---|fE
-					-- end
+					auto_show = true,
+					auto_show_delay_ms = 0,
+
+					window = {
+						border = "rounded",
+						min_width = 30,
+						winhighlight = ""
+					},
+					draw = function (data)
+						---|fS
+
+						---@type integer
+						local buf = data.window.buf;
+						---@type integer
+						local src_buf = vim.api.nvim_get_current_buf();
+
+						---@type string[]
+						local lines = {};
+
+						if data.item and data.item.documentation then
+							lines = vim.split(data.item.documentation.value or "", "\n", { trimempty = true });
+						end
+
+						---@type string[]
+						local details = vim.split(data.item.detail or "", "\n", { trimempty = true });
+
+						if #details > 0 then
+							table.insert(details, 1, string.format("```%s", vim.bo[src_buf].ft or ""));
+							table.insert(details, "```");
+
+							if #lines > 0 then
+								details = vim.list_extend(details, {
+									"",
+									"Detail: ",
+									"--------",
+									""
+								});
+							end
+						end
+
+						local visible_lines = vim.list_extend(details, lines);
+						vim.api.nvim_buf_set_lines(buf, 0, -1, false, visible_lines);
+
+						if vim.g.__reg_doc ~= true then
+							vim.treesitter.language.register("markdown", "blink-cmp-documentation");
+							vim.g.__reg_doc = true;
+						end
+
+						if package.loaded["markview"] then
+							local win = data.window:get_win();
+
+							if win then
+								vim.bo[buf].ft = "markdown";
+								require("markview").render(buf, { enable = true, hybrid_mode = false });
+								vim.bo[buf].ft = "blink-cmp-documentation";
+							end
+
+							vim.defer_fn(function ()
+								win = data.window:get_win();
+
+								if win then
+									vim.wo[win].signcolumn = "no";
+								end
+
+									vim.bo[buf].ft = "markdown";
+									require("markview").render(buf, { enable = true, hybrid_mode = false });
+									vim.bo[buf].ft = "blink-cmp-documentation";
+							end, 25);
+						end
+
+						---|fE
+					end,
 				},
-				-- signature = {
-				-- 	window = { border = 'single' }
-				-- }
 			},
 			keymap = {
 				preset = "none",

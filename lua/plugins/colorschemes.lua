bf9d8bdb360b99cdd4b5cc7d50416a733e3f2391
Shawon <mdmoinulhossainshawon@gmail.com>
2025-05-09 20:10:04 +0600

    refactor(colorschemes): Colorschemes are no longer transparent on Mac

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,3 @@
---- Load all the colorschemes(used for screenshots).
---- Lazy loading doesn't do anything significant here.
 return {
 	{
 		"catppuccin/nvim",
@@ -11,7 +9,6 @@ return {
 			require("catppuccin").setup({
 				flavour = "mocha",
 				no_italic = _G.is_within_termux(),
-				transparent_background = _G.is_within_termux() == false,
 
 				dim_inactive = {
 					enabled = true,
@@ -26,50 +23,42 @@ return {
 	},
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
+		lazy = true,
 		priority = 1000,
 
-		config = function ()
-			require("tokyonight").setup({
-			});
-		end,
+		opts = {}
 	},
 	{
 		"scottmckendry/cyberdream.nvim",
-		lazy = false,
+		lazy = true,
 		priority = 1000,
-		opts = {
-			-- Enable transparent background
-			transparent = true,
-		}
+
+		opts = {}
 	},
 	{
 		"bluz71/vim-nightfly-colors",
 		name = "nightfly",
-		lazy = false,
+		lazy = true,
 		priority = 1000
 	},
 	{
 		"craftzdog/solarized-osaka.nvim",
-		lazy = false,
+		lazy = true,
 		priority = 1000,
 
 		opts = {
-			transparent = true, -- Disable setting background
+			transparent = false,
 			dim_inactive = true, -- Non focused panes set to alternative background
-			styles = {
-				sidebars = "transparent",
-			},
 		}
 	},
 	{
 		"olimorris/onedarkpro.nvim",
-		lazy = false,
+		lazy = true,
 		priority = 1000
 	},
 	{
 		"rebelot/kanagawa.nvim",
-		lazy = false,
+		lazy = true,
 		priority = 1000
 	}
 };

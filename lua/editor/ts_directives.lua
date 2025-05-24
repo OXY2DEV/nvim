0ab357640fc5b63ba6ddc5ce4bb62323b6f3fbaf
Shawon <mdmouinulhossainshawon@gmail.com>
2025-05-24 08:00:08 +0600

    chore(parsers): Updated quickfix tree-sitter parser

--- lua/editor/ts_directives.lua
+++ lua/editor/ts_directives.lua
@@ -23,4 +23,43 @@ vim.treesitter.query.add_directive("set-qf-lang!", function (match, _, bufnr, pr
 	};
 
 	metadata["injection.language"] = ft or exceptions[delimiter] or "";
-end, { force = true, all = false });
+end, { force = true });
+
+if true then
+	-- No need to load the next one. We don't use it at-the-moment!
+	return;
+end
+
+vim.treesitter.query.add_directive("qf-fallback-lang!", function (match, _, bufnr, pred, metadata)
+	local capture_id = pred[2];
+	---@type TSNode
+	local node = match[capture_id];
+
+	if not node then
+		-- Couldn't find node 
+		return;
+	end
+
+	local parent = node:parent();
+
+	if not parent then
+		-- Couldn't find the list item node.
+		return;
+	end
+
+	local filename = parent:child(0);
+
+	if not filename or filename:type() ~= "filename" then
+		-- Filename node doesn't exist or has wrong type.
+		return;
+	end
+
+	local text = vim.treesitter.get_node_text(filename, bufnr);
+	local ft = vim.filetype.match({ filename = text })
+
+	if ft then
+		-- Only add it if the filetype is found.
+		metadata["injection.language"] = ft;
+	end
+end, { force = true });
+
--- queries/qf/injections.scm
+++ queries/qf/injections.scm
@@ -2,3 +2,12 @@
   (language_delimiter) @_lang
   (content) @injection.content
   (#set-qf-lang! @_lang))
+
+; Uncomment the lines below if you want syntax highlighting
+; for the default quickfix list.
+;
+; ((code_block
+;   .
+;   (content) @injection.content
+;   .) @_qf
+;   (#qf-fallback-lang! @_qf))

9560537f467a05cf9c937a660ae11484d5ea1546
Shawon <mdmouinulhossainshawon@gmail.com>
2025-06-11 09:03:55 +0600

    chore(inspect): Added more node support for lua patterns

--- lua/scripts/node_maps.lua
+++ lua/scripts/node_maps.lua
@@ -57,8 +57,19 @@ node_maps.named = {
 		["^func"] = {
 			icon = "󰡱 ",
 			icon_hl = "@function"
-		}
-	}
+		},
+	},
+
+	lua_patterns = {
+		pattern = {
+			icon = "󰛪 ",
+			icon_hl = "@property"
+		},
+		literal_character = {
+			icon = "󰾽 ",
+			icon_hl = "@string"
+		},
+	},
 };
 
 return node_maps;

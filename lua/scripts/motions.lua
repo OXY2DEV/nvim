8abb501ba246fa4be76d76003c16b9cb805e4a63
Shawon <mdmouinulhossainshawon@gmail.com>
2025-06-26 07:29:08 +0600

    refactor(motions): Changed how motions are detected

--- lua/scripts/motions.lua
+++ lua/scripts/motions.lua
@@ -9,129 +9,220 @@ motions.ns = vim.api.nvim_create_namespace("motions");
 ---@type string Previous keys. Must be cleared after each motion!
 motions.previous = "";
 
-motions.parse = function (map, key, _)
-	map = map or {};
+---@type string Last successfully run motion.
+motions.last = "";
 
-	if motions.mode ~= "n" then
-		-- Wrong mode.
-		motions.previous = "";
-		return;
-	end
-
-	if motions.previous == "" then
+--[[
+	if string.match(motions.previous, "^%d*$") then
 		if key == "." then
-			pcall(map[key], key);
+			-- Dot repeat.
+			exec_callback(true, ".", "dot_repeat", motions.last);
 		elseif key == "~" then
-			pcall(map[key], key);
+			-- Case change under cursor.
+			exec_callback(true, "~", "case_swap");
 		elseif key == "|" then
-			pcall(map[key], key);
+			-- To [0]th screen col.
+
+			---@type integer
+			local count = tonumber(motions.previous) or 0;
+
+			exec_callback(true, "|", "case_swap", count);
 		elseif key == ";" then
-			pcall(map[key], key);
+			-- Repeat last f, t, F, T.
+
+			exec_callback(true, ";", "repeat_find_character");
 		elseif key == "," then
-			pcall(map[key], key);
+			-- Repeat last f, t, F, T in opposite direction.
+
+			exec_callback(true, ",", "repeat_find_character_reverse");
 		elseif key == "G" then
-			pcall(map[key], key);
+			-- Bottom of file.
+
+			exec_callback(true, "G", "to_bottom");
 		elseif key == "-" or key == "+" then
-			pcall(map[key], key);
+			-- Use -, + to move up/down.
+
+			exec_callback(true, key, "quantified_movement", key);
 		elseif key == "w" or key == "W" then
-			pcall(map[key], key);
+			-- Use w, W to move between to words.
+
+			exec_callback(true, key, "to_word", key);
 		elseif key == "e" or key == "E" then
-			pcall(map[key], key);
+			-- Use e, E to move between to end of words.
+
+			exec_callback(true, key, "to_word_end", key);
 		elseif key == "b" or key == "B" then
-			pcall(map[key], key);
+			-- Use b, B to move between to start of words.
+
+			exec_callback(true, key, "to_word_start", key);
 		elseif key == "(" or key == ")" then
-			pcall(map[key], key);
+			-- Use (, ) to move between sentences.
+
+			exec_callback(true, key, key == ")" and "sentence_next" or "sentence_previous", key);
 		elseif key == "{" or key == "}" then
-			pcall(map[key], key);
+			-- Use {, } to move between paragraph.
+
+			exec_callback(true, key, key == "}" and "paragraph_next" or "paragraph_previous", key);
 		elseif vim.list_contains({ "H", "M", "L" }, key) then
-			pcall(map[key], key);
+			-- Use H, M, L to go to specific part of the window.
+
+			exec_callback(true, key, "to_window_line", key);
 		elseif vim.list_contains({ "h", "j", "k", "l" }, key) then
-			pcall(map[key], key);
+			-- Use h, j, k, l to move around the buffer.
+
+			exec_callback(true, key, "to_buffer_line", key);
 		elseif vim.list_contains({ "i", "v", "V", "", ":" }, key) then
-			pcall(map[key], key);
+			-- Use v, V, , i, : to change mode.
+
+			exec_callback(true, key, "mode_change", key);
 		elseif vim.list_contains({ "0", "^", "$" }, key) then
-			pcall(map[key], key);
+			-- Use ^, 0, $ to move to specific parts of the line.
+
+			exec_callback(true, key, "to_buffer_column", key);
 		else
 			motions.previous = motions.previous .. key;
 		end
-	elseif motions.previous == "]" then
+	elseif string.match(motions.previous, "%]$") then
 		if key == "]" then
-			pcall(map["]]"], "]]");
+			-- Use ] to go to next section.
+
+			exec_callback(true, "]", "section_next");
 		elseif key == "[" then
-			pcall(map["]["], "][");
+			-- Use ][ to go to next section.
+
+			exec_callback(true, "][");
 		elseif key == "'" then
-			pcall(map["]'"], "]'");
+			-- ???
+
+			exec_callback(true, "]'");
 		elseif key == "`" then
-			pcall(map["]`"], "]`");
+			-- ???
+
+			exec_callback(true, "]`");
 		elseif key == ")" then
-			pcall(map["])"], "])");
+			-- Use to go to unmatched ).
+
+			exec_callback(true, "])", "to_unmatch_bracket");
 		elseif key == "}" then
-			pcall(map["]}"], "]}");
+			-- Use to go to unmatched }.
+
+			exec_callback(true, "]}", "to_unmatch_paren");
 		elseif key == "m" then
-			pcall(map["]m"], "]m");
+			-- Use to go to start of method.
+
+			exec_callback(true, "]m");
 		elseif key == "M" then
-			pcall(map["]M"], "]M");
+			-- Use to go to end of method.
+
+			exec_callback(true, "]M");
 		elseif key == "*" or key == "/" then
-			pcall(map["]" .. key], "]" .. key);
+			-- Use ]* or ]/ to go to start of C comment.
+
+			exec_callback(true, "]" .. key, "to_c_comment");
 		end
 
+		-- Doesn't matter what gets run, buffer should
+		-- be cleared.
 		motions.previous = "";
-	elseif motions.previous == "[" then
+	elseif string.match(motions.previous, "%[$") then
 		if key == "[" then
-			pcall(map["[["], "[[");
+			-- Use [[ to go to previous section.
+
+			exec_callback(true, "[[", "section_previous");
 		elseif key == "]" then
-			pcall(map["[]"], "[]");
+			-- ???
+
+			exec_callback(true, "[]");
 		elseif key == "'" then
-			pcall(map["['"], "['");
+			-- ???
+
+			exec_callback(true, "['");
 		elseif key == "`" then
-			pcall(map["[`"], "[`");
+			-- ???
+
+			exec_callback(true, "[`");
 		elseif key == "(" then
-			pcall(map["[("], "[(");
+			-- ???
+
+			exec_callback(true, "[(");
 		elseif key == "{" then
-			pcall(map["[{"], "[{");
+			-- ???
+
+			exec_callback(true, "[{");
 		elseif key == "m" then
-			pcall(map["[m"], "[m");
+			-- ???
+
+			exec_callback(true, "[m");
 		elseif key == "M" then
-			pcall(map["[M"], "[M");
+			-- ???
+
+			exec_callback(true, "[M");
 		elseif key == "#" then
-			pcall(map["[#"], "[#");
+			-- ???
+
+			exec_callback(true, "[#");
 		elseif key == "*" or key == "/" then
-			pcall(map["[" .. key], "[" .. key);
+			-- ???
+
+			exec_callback(true, "[" .. key);
 		end
 
 		motions.previous = "";
 	elseif motions.previous == "}" then
 		if key == "#" then
-			pcall(map["}#"], "}#");
+			-- ???
+
+			exec_callback(true, "}#");
 		end
 
 		motions.previous = "";
 	elseif motions.previous == "'" then
 		if vim.list_contains({ "]", "[" }, key) then
-			pcall(map["'" .. key], "'" .. key);
+			-- ???
+
+			exec_callback(true, "'" .. key);
 		elseif vim.list_contains({ "<", ">" }, key) then
-			pcall(map["'" .. key], "'" .. key);
+			-- ???
+
+			exec_callback(true, "'" .. key);
 		elseif key == "'" then
-			pcall(map["''"], "''");
+			-- ???
+
+			exec_callback(true, "''");
 		elseif key == '"' then
-			pcall(map["'\""], "'\"");
+			-- ???
+
+			exec_callback(true, "'\"");
 		elseif key == "^" then
-			pcall(map["'^"], "'^");
+			-- ???
+
+			exec_callback(true, "'^");
 		elseif key == "." then
-			pcall(map["'."], "'.");
+			-- ???
+
+			exec_callback(true, "'.");
 		elseif key == "(" or key == ")" then
-			pcall(map["'" .. key], "'" .. key);
+			-- ???
+
+			exec_callback(true, "'" .. key);
 		elseif key == "{" or key == "}" then
-			pcall(map["'" .. key], "'" .. key);
+			-- ???
+
+			exec_callback(true, "'" .. key);
 		end
 
 		motions.previous = ""
-	elseif motions.previous == "g'" or motions.previous == "g`" then
-			pcall(map[motions.previous .. key], "'" .. motions.previous .. key);
+	elseif string.match(motions.previous, "g[`']") then
+		-- ???
+
+		exec_callback(true, motions.previous .. key);
 	elseif motions.previous == "g" then
 		if vim.list_contains({ "_", "^", "m", "M", "$" }, key) then
+			-- ???
+
+			exec_callback(true, motions.previous .. key);
+			pcall(map[motions.previous .. key], motions.previous .. key);
 			motions.previous = "";
-			pcall(map[motions.previous .. key], "'" .. motions.previous .. key);
 		elseif vim.list_contains({ "e", "E" }, key) then
 			motions.previous = "";
 			pcall(map[motions.previous .. key], "'" .. motions.previous .. key);
@@ -168,6 +259,85 @@ motions.parse = function (map, key, _)
 	else
 		motions.previous = motions.previous .. key;
 	end
+--]]
+
+motions.parse = function (map, key, _)
+	map = map or {};
+
+	local quantifier = tonumber(
+		string.match(motions.previous, "%d+$")
+	) or 0;
+
+	---@param clear boolean
+	---@param motion string
+	---@param alt? string
+	---@param ... any
+	local function exec_callback (clear, motion, alt, ...)
+		if type(map) ~= "table" then
+			return;
+		elseif clear then
+			motions.previous = "";
+		end
+
+		motions.last = motion;
+
+		if alt and map[alt] then
+			pcall(map[alt], ...);
+		else
+			pcall(map[motion], ...);
+		end
+	end
+
+	local single_keys = {
+		"h", "j", "k", "l",
+		"0", "^", "$", "|",
+		"G",
+		";", ",",
+		"w", "W", "e", "E", "b", "B",
+		"(", ")", "{", "}",
+		"%",
+		"H", "M", "L",
+
+
+		"n", "N",
+		"~",
+	};
+
+	if motions.mode ~= "n" then
+		-- Wrong mode.
+		motions.previous = "";
+	elseif motions.previous == "" and string.match(key, "%d") then
+		-- Quantifier.
+		motions.previous = motions.previous .. key;
+	elseif string.match(motions.previous, "%d*[fFtT]") then
+		-- Character finder.
+		exec_callback(true, string.match(motions.previous, "[fFtT]$"), nil, quantifier, key);
+	elseif vim.list_contains(single_keys, key) then
+		-- Single letter motions.
+		exec_callback(true, key, nil, quantifier);
+	elseif string.match(motions.previous, "g['`]$") then
+		exec_callback(true, "g" .. key, nil, quantifier);
+	elseif string.match(motions.previous, "g$") then
+		local actions = {
+			"_", "0", "^",
+			"m", "M",
+			"$",
+			"k", "j",
+			"g",
+			"e", "E",
+			";", ",",
+		};
+
+		if vim.list_contains(actions, key) then
+			exec_callback(true, "g" .. key, nil, quantifier);
+		elseif key == "'" or key == "`" then
+			motions.previous = motions.previous .. key;
+		else
+			motions.previous = "";
+		end
+	else
+		motions.previous = motions.previous .. key;
+	end
 end
 
 motions.event = function (map)
@@ -181,5 +351,7 @@ vim.api.nvim_create_autocmd("ModeChanged", {
 		motions.mode = event.match;
 	end
 });
+
+motions.event({});
 
 return motions;

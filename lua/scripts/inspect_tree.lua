7db37aae02217e99a941af86c8244078152747da
Shawon <mdmouinulhossainshawon@gmail.com>
2025-06-30 15:58:13 +0600

    refactor(inspect_tree): Moved config to seperate file

--- lua/scripts/inspect_tree.lua
+++ lua/scripts/inspect_tree.lua
@@ -33,67 +33,10 @@ inspect.config = {
 	named_only = false,
 	indent_size = 4,
 
-	injections = {
-		default = {
-			scope_hl = "MarkviewCode",
-			icon = "󱏒 ",
+	injections = require("scripts.tree_maps").injections,
 
-			text = nil,
-			hl = { "Injection0", "Injection1", "Injection2", },
-
-			icon_hl = "@constant",
-			text_hl = "@constant",
-		},
-
-		markdown_inline = {
-			icon = "󰂽 ",
-			text = "Markdown(inline)",
-
-			hl = "Injection6",
-			icon_hl = "@module",
-			text_hl = "@module"
-		},
-
-		["^lua$"] = {
-			icon = " ",
-			text = "Lua",
-
-			hl = "Injection5",
-			icon_hl = "@function",
-			text_hl = "@function"
-		},
-
-		luadoc = {
-			icon = " ",
-			text = "LuaDoc",
-
-			hl = "Injection0",
-			icon_hl = "@comment",
-			text_hl = "@comment"
-
-		},
-
-		lua_patterns = {
-			icon = " ",
-			text = "Lua patterns",
-
-			hl = "Injection2",
-			icon_hl = "@constant",
-			text_hl = "@constant"
-		},
-
-		yaml = {
-			icon = "󰨑 ",
-			text = "YAML",
-
-			hl = "Injection2",
-			icon_hl = "@constant",
-			text_hl = "@constant"
-		},
-	},
-
-	anonymous_nodes = require("scripts.node_maps").anon,
-	named_nodes = require("scripts.node_maps").named,
+	anonymous_nodes = require("scripts.tree_maps").anon,
+	named_nodes = require("scripts.tree_maps").named,
 };
 
 ---@param node TSNode
@@ -475,6 +418,7 @@ function inspector:open (buf)
 	vim.wo[self.win].conceallevel = 3;
 	vim.wo[self.win].concealcursor = "nvc";
 	vim.wo[self.win].signcolumn = "no";
+	vim.wo[self.win].list = true;
 
 	vim.w[self.win].inspecttree_window = true;
 

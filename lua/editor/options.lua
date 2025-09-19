6e2252d93b1fd22916f2065ae531769041bc7e06
Shawon <mdmoinulhossainshawon@gmail.com>
2025-09-19 07:40:11 +0600

    refactor: Updated comment styles
    feat: Added recommened markdown settings

--- lua/editor/options.lua
+++ lua/editor/options.lua
@@ -2,98 +2,97 @@
 
 ---|fS "doc: Neovim options"
 
---- Prefer dark background.
+-- Prefer dark background.
 vim.o.background = "dark";
 
---- Use breakindent for blocks of wrapped text.
+-- Use breakindent for blocks of wrapped text.
 vim.o.breakindent = true;
 
---- Cmdline height.
+-- Cmdline height.
 vim.o.cmdheight = 1;
---- For `q:`.
+-- For `q:`.
 vim.o.cmdwinheight = 5;
 
---- Useful when using multiple windows.
+-- Useful when using multiple windows.
 vim.o.cursorline = true;
 
---- Confirm before exiting.
+-- Confirm before exiting.
 vim.o.confirm = true;
 
---- Change the fill characters for fold, end of buffer and
---- last line.
+-- Change the **fill characters** for 'fold', `end of buffer` and *last line*.
 vim.o.fillchars = "fold: ,eob: ,lastline: ";
 
---- Use markers for folding.
+-- Use *markers* for `folding`.
 vim.o.foldmethod = "marker";
 vim.o.foldmarker = "|fS,|fE";
 
---- `:h` should be 25% of the total display height.
---- `cmdheight` is assumed to be 1.
+-- `:h` should be **25%** of the total display height.
+-- `cmdheight` is assumed to be **1**.
 vim.o.helpheight = math.ceil((vim.o.lines - 1) * 0.25);
 
---- Allow `listchars` to be shown.
+-- Allow `listchars` to be shown.
 vim.o.list = true;
 vim.o.listchars = "eol:↩,tab:│ ,trail:╴";
 
---- Only allow using mouse/touch for `Normal` mode.
+-- Only allow using mouse/touch for `Normal` mode.
 vim.o.mouse = "n";
---- Enable line numbers.
+-- Enable line numbers.
 vim.o.number = true;
 
---- Smaller completion window.
+-- Smaller completion window.
 vim.o.pumheight = 5;
---- Slightly transparent completion window.
+-- Slightly *transparent* completion window.
 vim.o.pumblend = 20;
 
---- Use relative line numbers.
+-- Use relative line numbers.
 vim.o.relativenumber = true;
 
---- BUG, Custom ruler format with custom statusline can mess
---- up incoming messages!
+-- BUG(messages, ruler): Custom `ruler format` with `custom statusline` can mess up incoming messages!
 vim.o.ruler = true;
 vim.o.rulerformat = " %l | %c";
 
---- I don't want to constantly go to the edge of the screen
---- just to scroll.
+-- I don't want to constantly go to the edge of the screen just to scroll.
 vim.o.scrolloff = 999;
 vim.o.sidescrolloff = 15;
 
---- Use 4 spaces per indent level.
+-- Use **4** spaces per indent level.
 vim.o.shiftwidth = 4;
 
---- Do not show partial commands. It bugs out on Termux.
---- Also slows down stuff in Visual mode.
---- vim.o.showcmd = false;
+-- Do not show partial commands. It bugs out on Termux.
+-- Also slows down stuff in Visual mode.
+-- vim.o.showcmd = false;
 
---- No point in showing the mode(use custom statusline).
---- vim.o.showmode = false;
+-- No point in showing the mode(use custom statusline).
+-- vim.o.showmode = false;
 
---- This isn't required. It's here just for completeness.
+-- This isn't required. It's here just for completeness.
 vim.o.softtabstop = 4;
 
---- Enable spelling.
---- I also use camelCase & don't want spell
---- on buffers with no syntax.
+-- Enable spelling.
+-- I also use camelCase & don't want spell on buffers with no syntax.
 vim.o.spell = true;
 vim.o.spelloptions = "camel,noplainbuffer";
 
---- `:sp` opens split below.
+-- `:sp` opens split below.
 vim.o.splitbelow = true;
---- `:vsp` opens split to the right.
+-- `:vsp` opens split to the right.
 vim.o.splitright = true;
 
---- Use 256-colors.
+-- Use `true colors`.
 vim.o.termguicolors = true;
 
---- Each tab counts for 4 spaces.
+-- Each tab counts for **4** spaces.
 vim.o.tabstop = 4;
 
---- I normally don't use wrap.
+-- I normally don't use `wrap`.
 vim.o.wrap = false;
+
+-- Use the recommended settings for `markdown`.
+vim.g.markdown_recommened_style = 1;
 
 ---|fE
 
---- Load a fallback colorscheme
+-- Load a fallback colorscheme
 if vim.fn.has("nvim-0.9.5") == 0 then
 	--[[
 		On versions older then 0.9.5 load `habamax`.
@@ -101,7 +100,7 @@ if vim.fn.has("nvim-0.9.5") == 0 then
 	vim.cmd.colorscheme("habamax");
 end
 
--- Configuration for diagnostics.
+-- Configuration for `diagnostics`.
 vim.diagnostic.config({
 	severity_sort = true,
 

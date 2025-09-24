687a5560d792a98abed5703891793331f482b2b8
Shawon <mdmoinulhossainshawon@gmail.com>
2025-09-24 18:50:59 +0600

    refactor(git): Disabled auto line blames

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -18,7 +18,7 @@ return {
 				changedelete = { text = "~" },
 				untracked    = { text = "┆" },
 			},
-			current_line_blame = true,
+			current_line_blame = false,
 			current_line_blame_opts = {
 				virt_text_pos = "right_align",
 			},

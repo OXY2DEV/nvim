ec480620fea131dd8bc0009956693487296c6deb
Shawon <mdmoinulhossainshawon@gmail.com>
2025-09-24 18:43:21 +0600

    refactor(plugins, tree-sitter)!: Switched to new parser registering method
    May not be compatible with `master` anymore.

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -13,76 +13,102 @@ return {
 		local function get_path(path, url)
 			---@type table Path stat.
 			local stat = vim.uv.fs_stat(vim.fn.stdpath("config") .. path);
+			local parsers = require("nvim-treesitter.parsers");
 
-			if stat and stat.type == "directory" then
+			if type(parsers.get_parser_configs) ~= "function" then
+				return url;
+			elseif stat and stat.type == "directory" then
 				return vim.fn.stdpath("config") .. path;
 			else
 				return url;
 			end
 		end
 
-		local parsers = require("nvim-treesitter.parsers");
-		local is_on_main = type(parsers.get_parser_configs) ~= "function";
+		---|fS "config: Parser info"
 
-		local parser_configs = parsers.get_parser_configs and parsers.get_parser_configs() or parsers;
+		local update_stack = {};
 
-		parser_configs.doctext = {
+		local function new_parser (name, opts)
+			local parsers = require("nvim-treesitter.parsers");
+
+			if type(parsers.get_parser_configs) ~= "function" then
+				opts.install_info.path = vim.fn.stdpath("config") .. "/parsers/tree-sitter-" .. name;
+				opts.install_info.queries = "queries/";
+				opts.install_info.generate = true;
+
+				update_stack[name] = opts;
+			else
+				require("nvim-treesitter.parsers").get_parser_configs()[name] = opts;
+			end
+		end
+
+		new_parser("doctext", {
 			install_info = {
 				url = get_path("/parsers/tree-sitter-doctext", "https://github.com/OXY2DEV/tree-sitter-doctext"),
 				files = { "src/parser.c" }
 			}
-		};
+		});
 
-		parser_configs.lua_patterns = {
+		new_parser("lua_patterns", {
 			install_info = {
 				url = get_path("/parsers/tree-sitter-lua_patterns", "https://github.com/OXY2DEV/tree-sitter-lua_patterns"),
 				files = { "src/parser.c" }
 			}
-		};
+		});
 
-		parser_configs.vhs = {
+		new_parser("vhs", {
 			install_info = {
 				url = get_path("/parsers/tree-sitter-vhs", "https://github.com/OXY2DEV/tree-sitter-vhs"),
 				files = { "src/parser.c" }
 			}
-		};
+		});
 
-		parser_configs.qf = {
+		new_parser("qf", {
 			install_info = {
 				url = get_path("/parsers/tree-sitter-qf", "https://github.com/OXY2DEV/tree-sitter-qf"),
 				files = { "src/parser.c" }
 			},
-			filetype = "qf"
-		};
+		});
 
-		parser_configs.kitty = {
+		new_parser("kitty", {
 			install_info = {
 				url = get_path("/parsers/tree-sitter-kitty", "https://github.com/OXY2DEV/tree-sitter-kitty"),
 				files = { "src/parser.c" }
 			},
-			filetype = "kitty"
-		};
+		});
 
+		---@type string[]
 		local use_parsers = {
 			"vim",
 			"lua",
 
+			"vimdoc",
+			"luadoc",
+			"doctext",
+
+			"lua_patterns",
 			"regex",
+
 			"html",
 			"markdown",
 			"markdown_inline",
 			"yaml",
 
-			"vimdoc",
 			"query",
-
-			"lua_patterns",
 			"vhs",
-
-			"python"
 		};
 
-		if is_on_main then
+		---|fE
+
+		if require("nvim-treesitter.parsers").get_parser_configs == nil then
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "TSUpdate",
+				callback = function ()
+					for k, v in pairs(update_stack) do
+						require("nvim-treesitter.parsers")[k] = v;
+					end
+				end
+			});
 			require("nvim-treesitter").install(use_parsers);
 
 			vim.api.nvim_create_autocmd("Filetype", {
